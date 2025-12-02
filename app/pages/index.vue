12e6a55215971f994537647734250fadec217596
Denki <bardey.estelle1@gmail.com>
2025-12-02 11:42:01 +0100

    Ajout des filtres

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,12 +7,49 @@ const { data : recipes, error } = await useAsyncData('recipes',async () => {
   )
   return data
 })
+const { data : cuisines } = await useAsyncData('cuisines',async () => {
+  const { data } = await $fetch<APIReseponse<Cuisine[]>>(
+    `${config.public.apiUrl}/api/cuisines`
+  )
+  return data
+})
+
+function onCheckboxInput($event: Event) {
+    const target = $event.target
+    if(!(target instanceof HTMLInputElement)) return
+    const value = target.value
+    if (!filters.value.includes(value)) {
+        filters.value.push(value)
+    } else {
+        const index = filters.value.findIndex((v => v === value))
+        filters.value.splice(index, 1)
+    }
+}
+
+const filters = ref<Cuisine['name'][]>([])
+
+const filteredRecipes = computed(() => {
+  if (!recipes.value) return []
+  if (!filters.value.length) return recipes.value
+  return recipes.value.filter(recipe => filters.value.includes(recipe.cuisine_name))
+})
+
+
+
+if (error && error.value) throw new Error('Failed to fetch recipes')
 </script>
 
 <template>
   <div>
     <h1>Home</h1>
-    <div v-for="(recipe, index) in recipes" :key="index" class="card--test">
+    <h2>Filter by Cuisine</h2>
+    <div class="recipes__filter">
+        <div v-for="cuisine in cuisines" :key="cuisine.cuisine_id">
+          <label for="">{{ cuisine.name }}</label>
+          <input type="checkbox" :value="cuisine.name" @input="onCheckboxInput" />
+        </div>
+    </div>
+    <div v-for="(recipe, index) in filteredRecipes" :key="index" class="card--test">
       <NuxtLink :to="`/recipe/${recipe.recipe_id}`">
         <img :src="`/images/${recipe.image_url}`" alt="Recipe Image">
         <h2>{{ recipe.title }}</h2>
