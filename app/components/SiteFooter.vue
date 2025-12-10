1bf55b05e535c8377e341d838ed44e09acf50ce6
Denki <bardey.estelle1@gmail.com>
2025-12-10 09:17:08 +0100

    Ajout de styles pour la version desktop du footer

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -5,40 +5,44 @@
       <img class="footer__logo" src="/images/MainLogoDarkMode.png" alt="Logo" >
       <p class="footer__rights">All Rights Reserved</p>
     </div>
-    <div class="footer__section_FreshRecipe">
-      <h3>Fresh Recipes</h3>
-      <ul>
-        <li>Recipes</li>
-        <li>Winter salads</li>
-        <li>Organic chicken</li>
-        <li>Beef and Mutton</li>
-        <li>Flavoured Milk</li>
-      </ul>
-    </div>
-    <div class="footer__section_news">
-      <h3>In News</h3>
-      <ul>
-        <li>Our Blogs</li>
-        <li>Contests/Sweepsatkes</li>
-        <li>Video</li>
-        <li>New Releases</li>
-        <li>Newsletters</li>
-      </ul>
-    </div>
-    <div class="footer__section_AboutUs">
-      <h3>About US</h3>
-      <ul>
-        <li>FAQ</li>
-        <li>Our Board</li>
-        <li>Our Staff</li>
-        <li>Contact Us</li>
-      </ul>
-    </div>
-    <div class="footer__section_newsletter">
-      <h3>Juin Our Newsletter</h3>
-      <div class="footer__newsletter-input">
-        <input type="email" placeholder="email">
-        <button>Subscribe</button>
+    <div class="footer__sectionRight">
+      <div class="footer__section_FreshRecipe">
+        <h3>Fresh Recipes</h3>
+        <ul>
+          <li>Recipes</li>
+          <li>Winter salads</li>
+          <li>Organic chicken</li>
+          <li>Beef and Mutton</li>
+          <li>Flavoured Milk</li>
+        </ul>
+      </div>
+      <div class="footer__section_news">
+        <h3>In News</h3>
+        <ul>
+          <li>Our Blogs</li>
+          <li>Contests/Sweepsatkes</li>
+          <li>Video</li>
+          <li>New Releases</li>
+          <li>Newsletters</li>
+        </ul>
+      </div>
+      <div class="footer__section_AboutUs">
+        <h3>About US</h3>
+        <ul>
+          <li>FAQ</li>
+          <li>Our Board</li>
+          <li>Our Staff</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div class="footer__section_newsletter">
+        <h3>Juin Our Newsletter</h3>
+        <div class="footer__newsletter-input">
+          <input type="email" placeholder="email">
+          <button>
+            <AssetsSendMail />
+          </button>
+        </div>
       </div>
     </div>
   </footer>
