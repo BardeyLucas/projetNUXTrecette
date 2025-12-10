bcbd92124cbedf10f30fc67da64673814a4a698e
Denki <bardey.estelle1@gmail.com>
2025-12-10 16:03:26 +0100

    Mise à jour du aside

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -70,26 +70,72 @@ watch(() => [filters.value, search.value], () => {
 </script>
 
 <template>
-  <div>
-    <h1>Home</h1>
-    <h2>Filter by Cuisine</h2>
-    <input v-model="search" type="text" placeholder="Search..." >
-    <p>{{ search }}</p>
-    <p>Page {{ page }} / {{ totalPages }}</p>
-    <input v-model="page" type="number" min="1" :max="totalPages" >
-    <div class="recipes__filter">
-      <div v-for="cuisine in cuisines" :key="cuisine.cuisine_id">
-        <label for="">{{ cuisine.name }}</label>
-        <input type="checkbox" :value="cuisine.name" @input="onCheckboxInput">
+  <div class="homePage">
+    <aside class="homePage__aside">
+      <h2 class="homePage__aside_title">Recherche par Plat</h2>
+      <input v-model="search" type="text" placeholder="Search..." class="homePage__aside_search" >
+      <h2 class="homePage__aside_title">Filtre par Cuisine</h2>
+      <div class="homePage__aside_filter">
+        <div class="homePage__aside_filter_item" v-for="cuisine in cuisines" :key="cuisine.cuisine_id">
+          <input class="homePage__aside_filter_item_input" :id="`cuisine-${cuisine.cuisine_id}`" type="checkbox" :value="cuisine.name" @input="onCheckboxInput">
+          <label class="homePage__aside_filter_item_label" :for="`cuisine-${cuisine.cuisine_id}`">{{ cuisine.name }}</label>
+        </div>
       </div>
-    </div>
-    <div v-for="(recipe, index) in paginatedRecipes" :key="index" class="card--test">
-      <NuxtLink :to="`/recipe/${recipe.recipe_id}`">
-        <img :src="`/images/${recipe.image_url}`" alt="Recipe Image">
-        <h2>{{ recipe.title }}</h2>
-        <div class="test__info"><p>{{ recipe?.cuisine_name }}</p><p>{{ recipe?.goal_name }}</p></div><div><p>{{ recipe?.diet_name }}</p><p>{{ recipe?.allergy_name }}</p></div>
-      </NuxtLink>
-    </div>
-    <p>Data des recettes {{ recipes }} {{ recipeError }}</p>
+    </aside>
+    <section>
+      <p>{{ search }}</p>
+      <div v-for="(recipe, index) in paginatedRecipes" :key="index" class="card--test">
+        <NuxtLink :to="`/recipe/${recipe.recipe_id}`">
+          <img :src="`/images/${recipe.image_url}`" alt="Recipe Image">
+          <h2>{{ recipe.title }}</h2>
+          <div class="test__info"><p>{{ recipe?.cuisine_name }}</p><p>{{ recipe?.goal_name }}</p></div><div><p>{{ recipe?.diet_name }}</p><p>{{ recipe?.allergy_name }}</p></div>
+        </NuxtLink>
+      </div>
+      <p>Page {{ page }} / {{ totalPages }}</p>
+      <input v-model="page" type="number" min="1" :max="totalPages" >
+      <p>Data des recettes {{ recipes }} {{ recipeError }}</p>
+    </section>
   </div>
 </template>
+<style lang="scss">
+  .homePage{
+    display: flex;
+
+    &__aside{
+      margin: rem(32) rem(100);
+
+      &_title{
+        font-family: Montez;
+        font-size: rem(40);
+        color: #84BD00;
+        line-height: 1;
+        margin-bottom: rem(20);
+      }
+
+      &_search{
+        width: rem(200);
+        height: rem(30);
+        border: rem(2) solid #84BD00;
+        border-radius: rem(8);
+        padding: rem(8);
+        margin-bottom: rem(32);
+      }
+
+      &_filter {
+        display: flex;
+        flex-direction: column;
+        gap: rem(5) rem(20);
+        margin-bottom: rem(20);
+
+        &_item_input {
+          display: none;
+        }
+
+        &_item_input:checked + &_item_label {
+          color: #84BD00;
+          font-weight: bolder;
+        }
+      }
+    }
+  }
+</style>
